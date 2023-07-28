15592673720a7e7758ebe2c297599a3957c1ebbb
PhamTHHanh <103091745+PhamTHHanh@users.noreply.github.com>
2023-07-28 15:00:07 +0700

    Fix conflix

--- lms/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ lms/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,91 +5,6 @@ export type AmplifyDependentResourcesAttributes = {
       "ApiName": "string",
       "RootUrl": "string"
     }
-<<<<<<< HEAD
-  },
-  "auth": {
-    "lmsbc7a393d": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
-  "function": {
-    "UserCourse": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "categories": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "courses": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "lectures": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  },
-  "storage": {
-    "Categories": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
-    },
-    "Lectures": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
-    },
-    "UserCourse": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "SortKeyName": "string",
-      "SortKeyType": "string",
-      "StreamArn": "string"
-    },
-    "Videos": {
-      "BucketName": "string",
-      "Region": "string"
-    },
-    "courses": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
-    }
-  }
-||||||| ef4cf25
-=======
   },
   "auth": {
     "lmsbc7a393d": {
@@ -205,5 +120,4 @@ export type AmplifyDependentResourcesAttributes = {
       "Region": "string"
     }
   }
->>>>>>> dbbfc950ffdcefa1af5b96be19cd8a390cecc131
 }
